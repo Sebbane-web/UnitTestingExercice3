28cbf0ea5fe7fa234bcf02e64299e8d8620896ca
Sebbane-web <sebbane.89@gmail.com>
2020-01-17 12:42:32 +0100

    Initial commit

---
+++ src/test/java/testRectangle.java
@@ -0,0 +1,7 @@
+package exercices;
+public class testRectangle {
+    public static void main (String [] args){
+        Rectangle monRectangle = new Rectangle (4.0,5.0);
+        monRectangle.afficher();
+    }
+}
